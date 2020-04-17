b63ab314fb29315ab5cbf54f344b2a89ad4e031d
cerwyn <cerwyneliata.c@gmail.com>
2020-04-17 16:04:26 +0700

    Initial Commit

---
+++ database/migrations/2020_04_16_002219_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('article_id')->unsigned();;
+            $table->string('text');
+            $table->integer('star');
+            $table->timestamps();
+            $table->foreign('article_id')
+                    ->references('id')
+                    ->on('articles')
+                    ->onCascade('delete');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+Route::post('article','ArticleController@store');
+Route::get('article/{article}','ArticleController@show');
+Route::get('article/{article}/comments','ArticleController@show_comments');
+Route::get('article/{article}/best-comment','ArticleController@show_best_comment');
+Route::get('articles','ArticleController@index');
+Route::delete('article/{article}','ArticleController@destroy');
+
+Route::post('article/{article}/comment','CommentController@store');
+Route::post('comment/{comment}/best-comment','CommentController@best_comment');
+Route::get('comments','CommentController@index');
+Route::get('comment/{comment}', 'CommentController@show');
+Route::delete('comment/{comment}','CommentController@destroy');
+
+Route::fallback(function(){
+    return response()->json([
+        'message' => 'API Not Found'], 404);
+});
